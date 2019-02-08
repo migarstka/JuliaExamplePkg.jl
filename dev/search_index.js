17c72775e96c2e76f91da1cf4d7484271ac3b656
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-02-08 17:21:51 +0000

    build based on 3ced381

---
+++ dev/search_index.js
@@ -0,0 +1,51 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "config.html#",
+    "page": "Config",
+    "title": "Config",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "config.html#Config-1",
+    "page": "Config",
+    "title": "Config",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "config.html#Config-Details-Level-2-1",
+    "page": "Config",
+    "title": "Config-Details Level 2",
+    "category": "section",
+    "text": "This describes the configuration of the package."
+},
+
+{
+    "location": "index.html#",
+    "page": "JuliaExamplePkg.jl Documentation",
+    "title": "JuliaExamplePkg.jl Documentation",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#JuliaExamplePkg.solve_problem-Tuple{}",
+    "page": "JuliaExamplePkg.jl Documentation",
+    "title": "JuliaExamplePkg.solve_problem",
+    "category": "method",
+    "text": "solve_problem()\n\nA simple test function used in this Package. Actually even this here is just used to test docstrings.\n\n\n\n\n\n"
+},
+
+{
+    "location": "index.html#JuliaExamplePkg.jl-Documentation-1",
+    "page": "JuliaExamplePkg.jl Documentation",
+    "title": "JuliaExamplePkg.jl Documentation",
+    "category": "section",
+    "text": "solve_problem()"
+},
+
+]}
